6d7d44da68f04aaf8e1f379984ba74fedbfb0885
Melissa Lawrence <mlawrence537@gmail.com>
2021-04-12 11:12:47 -0400

    finished deliverables

--- src/index.js
+++ src/index.js
@@ -13,3 +13,77 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+function renderOneToy(toyObj) {
+  const toyCard = document.createElement('div')
+  toyCard.classList.add('card')
+  toyCard.dataset.id = toyObj.id 
+
+  toyCard.innerHTML = `<h2>${toyObj.name}</h2>
+  <img src=${toyObj.image} width="250" height="300"/>
+  <p>${toyObj.likes} Likes </p>
+  <button class="like-btn">Like <3</button>`
+
+  const toyCollection = document.querySelector('div#toy-collection')
+  toyCollection.append(toyCard)
+}
+
+function renderAllToys() {
+  fetch('http://localhost:3000/toys')
+    .then((response) => response.json())
+    .then((toysArr) => {
+      toysArr.forEach(toyObject => {
+        renderOneToy(toyObject)
+      })
+    })
+}
+
+const newToyForm = document.querySelector('form.add-toy-form')
+
+newToyForm.addEventListener('submit', event => {
+  event.preventDefault() 
+
+  const newToyObj = {
+    name: event.target.name.value,
+    image: event.target.image.value,
+    likes: 0
+  }
+
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+  "Content-Type": "application/json",
+  "Accept": "application/json"
+  },
+  body: JSON.stringify(newToyObj)
+  })
+  .then(response => response.json())
+  .then(newToyData => { renderOneToy(newToyData)})
+
+  form.reset()
+
+})
+
+const allToysDiv = document.querySelector('div#toy-collection')
+
+allToysDiv.addEventListener('click', event => {
+  const toyDiv = event.target.closest('div')
+  const likesP = event.target.previousElementSibling
+  const currLikes = parseInt(likesP.textContent)
+  likesP.textContent = `${currLikes + 1} Likes`
+  
+  fetch(`http://localhost:3000/toys/${toyDiv.dataset.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      likes: currLikes + 1
+    })
+  })
+})
+
+
+renderAllToys()
